7336283458110de5ac2a70d443f1ac218cc8c2b9
thesamovar <dg.github@thesamovar.net>
2018-09-12 15:33:51 +0100

    minor fix

--- run-benchmarks-with-docker.sh
+++ run-benchmarks-with-docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run --runtime=nvidia -v `pwd`/:/root/brian2genn_benchmarks/ -it brian2genn_benchmarks /bin/bash /root/brian2genn_benchmarks/run-benchmarks-in-docker.sh
+sudo docker run --runtime=nvidia -v `pwd`/:/root/brian2genn_benchmarks/ -it brian2genn_benchmarks /bin/bash /root/brian2genn_benchmarks/run_all.sh
