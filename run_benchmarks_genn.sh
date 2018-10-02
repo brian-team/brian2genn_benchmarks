e66265fe1e56ccdb498a601412d261c74765e7e2
Marcel Stimberg <marcel.stimberg@inserm.fr>
2018-10-02 17:21:54 +0200

    fix output in run_benchmarks_genn script

--- run_benchmarks_genn.sh
+++ run_benchmarks_genn.sh
@@ -2,12 +2,10 @@
 
 if [ "$3" == "test" ]; then
     SCALING="1 8"
-    MONITORS="false"
     RUNTIME=1
     N_REPEATS=1
 else
     SCALING="0.05 0.1 0.25 0.5 1 2 4 8 16 32"
-    MONITORS="false"
     RUNTIME=10
     N_REPEATS=3
     if [ "$1" = "COBAHH.py" ]; then
@@ -25,6 +23,7 @@ else
     fi
 fi
 
+MONITORS="false"
 kernel_timing=false
 
 for monitor in $MONITORS; do
@@ -32,7 +31,7 @@ for monitor in $MONITORS; do
         for scaling in $SCALING; do
             for float_dtype in float32 float64; do
                 for repeat in $(seq $N_REPEATS); do
-                    echo Repeat $repeat: python $1 $scaling genn 0 1 $monitor $float_dtype $2 $para $kernel_timing
+                    echo Repeat $repeat: python $1 $scaling genn 0 $RUNTIME $monitor $float_dtype $2 $para $kernel_timing
                     python $1 $scaling genn 0 $RUNTIME $monitor $float_dtype $2 $para $kernel_timing
                     rm -r GeNNworkspace
                 done
